486789007d59df762854636eb8e7746756501f4d
이상훈 <162939838+tnwlvos@users.noreply.github.com>
2025-01-24 11:44:57 +0900

    Update apps.py

--- my-app/backend/accounts/apps.py
+++ my-app/backend/accounts/apps.py
@@ -1,6 +1,10 @@
 from django.apps import AppConfig
 
-
+# Django 애플리케이션을 설정하기 위한 클래스 정의
 class AccountsConfig(AppConfig):
+    # 기본적으로 사용할 자동 필드 유형 설정
+    # BigAutoField는 64비트 정수를 기본 키로 사용하는 필드
     default_auto_field = 'django.db.models.BigAutoField'
+    # 애플리케이션의 이름을 설정
+    # 이 이름은 settings.py의 INSTALLED_APPS에 추가된 이름과 일치해야 함
     name = 'accounts'
